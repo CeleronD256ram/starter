10a80eaab054c0ab3225914115d038e1b5571f94
CeleronD256ram <celerond@outlook.com>
2025-08-03 19:22:23 -0300

    Inicia o 'omnisharp', o servidor de linguagem C#

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,5 +2,13 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
+-- omnisharp languageserver
+local pid = vim.fn.getpid()
 
+lspconfig.omnisharp.setup({
+   cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(pid) },
+   on_attach = nvlsp.on_attach,
+   on_init = nvlsp.on_init,
+   capabilities = nvlsp.capabilities,
+})
 -- read :h vim.lsp.config for changing options of lsp servers 
