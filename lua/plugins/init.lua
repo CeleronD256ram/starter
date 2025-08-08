c986024bcd3d95819996662d8455718fdc1a9c02
CeleronD256ram <celerond@outlook.com>
2025-08-07 23:30:22 -0300

    instalação conform.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,66 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+   {
+      "stevearc/conform.nvim",
+      -- event = 'BufWritePre', -- uncomment for format on save
+      opts = require "configs.conform",
+   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+   -- These are some examples, uncomment them if you want to see them work!
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         require ("nvchad.configs.lspconfig").defaults()
+         require "configs.lspconfig"
+      end,
+   },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+   {
+      "williamboman/mason.nvim",
+      opts = {
+         ensure_installed = {
+            "lua-language-server",
+            "stylua",
+            "html-lsp",
+            "css-lsp",
+            "prettier",
+            "omnisharp",
+         },
+      },
+   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+   { -- This plugin
+      "Zeioth/compiler.nvim",
+      cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+      dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+      opts = {},
+   },
+   { -- The task runner we use
+      "stevearc/overseer.nvim",
+      commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+      cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+      opts = {
+         task_list = {
+            direction = "bottom",
+            min_height = 25,
+            max_height = 25,
+            default_detail = 1
+         },
+      },
+   },
+
+
+
+
+   -- test new blink
+   -- { import = "nvchad.blink.lazyspec" },
+
+   {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+         ensure_installed = {
+            "vim", "lua", "vimdoc",
+            "html", "css", "c_sharp"
+         },
+      },
+   },
 }
