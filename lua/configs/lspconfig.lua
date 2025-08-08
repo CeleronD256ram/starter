44f778539ed4571f12bce3b274ac39eafa39e3c6
CeleronD256ram <celerond@outlook.com>
2025-08-07 22:32:05 -0300

    mudança na implementação do lsp csharp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,14 +1,33 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
--- omnisharp languageserver
-local pid = vim.fn.getpid()
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-lspconfig.omnisharp.setup({
-   cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(pid) },
-   on_attach = nvlsp.on_attach,
-   on_init = nvlsp.on_init,
-   capabilities = nvlsp.capabilities,
-})
+local lspconfig = require "lspconfig"
+local servers = {
+  "bashls",
+  "dockerls",
+  "jsonls",
+  "lua_ls",
+  "marksman",
+  "sqlls",
+  "tailwindcss",
+  "yamlls",
+}
+
+for _, lsp in ipairs(servers) do
+   lspconfig[lsp].setup {
+      on_attach = on_attach,
+      on_init = on_init,
+      capabilities = capabilities,
+   }
+end
+
+lspconfig.csharp_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  root_dir = require('lspconfig.util').root_pattern("*.sln")
+}
 -- read :h vim.lsp.config for changing options of lsp servers 
