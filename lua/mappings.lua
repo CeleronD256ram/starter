1f49b586ff14dcb8fd121bf3d89204f4b0d32c18
CeleronD256ram <celerond@outlook.com>
2025-08-08 01:04:04 -0300

    novo atalho para abrir menu do compiler

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,20 @@
 require "nvchad.mappings"
 
--- add yours here
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Open compiler
+vim.api.nvim_set_keymap('n', '<leader>cc', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+
+-- Redo last selected option
+vim.api.nvim_set_keymap('n', '<S-F6>',
+   "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+   .. "<cmd>CompilerRedo<cr>",
+   { noremap = true, silent = true })
+
+-- Toggle compiler results
+vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
